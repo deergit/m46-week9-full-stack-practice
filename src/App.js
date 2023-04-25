7f4e1a39d4313ba7150696829ae8040b10574c8d
deergod <CommonTroll@live.co.uk>
2023-04-25 14:36:48 +0100

    setting up login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-import Login from './components/Login';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Login from "./components/Login";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState();
+
   return (
     <div className="App">
-      <Login />
+      <Login newUser={setUser}/>
+      {user ?
+        <h2>Welcome {user}</h2>
+        :
+        <h2>Please log in</h2>
+      }
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,33 @@
-const Login = (props) => {
+import { useState } from "react";
+import "./Login.css";
+
+const Login = ({ newUser }) => {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  
+  const submitHandler = () => {
+    //
+  }
+  
   return (
     <div className="login-container">
-      <h1>This is the login!</h1>
+      <h1>Login below:</h1>
+      <form onSubmit={submitHandler}>
+        <label>Username:<br />
+          <input onChange={(e) => setUsername(e.target.value)}></input>
+        </label>
+
+        <label>Email:<br />
+          <input onChange={(e) => setEmail(e.target.value)}></input>
+        </label>
+
+        <label>Password:<br />
+          <input onChange={(e) => setPassword(e.target.value)}></input>
+        </label>
+
+        <button type="submit">login</button>
+      </form>
     </div>
   );
 }
